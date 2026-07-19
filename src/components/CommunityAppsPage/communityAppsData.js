20085c2989165abfae97223bfa8eef8e6757a2a6
VeloSec <charles@velosec.au>
2026-07-19 14:04:36 +1000

    Update communityAppsData.js
    Add Voca to Community Apps

--- src/components/CommunityAppsPage/communityAppsData.js
+++ src/components/CommunityAppsPage/communityAppsData.js
@@ -1,5 +1,13 @@
 export const communityApps = [
   {
+    name: 'Voca Player',
+    platforms: ['iOS', 'iPadOS', 'watchOS', 'tvOS', 'Android', 'AAOS'],
+    href: 'https://voca.velosec.au',
+    tags: ['Audiobooks', 'Podcasts'],
+    description:
+      'A native audiobook and podcast player for iPhone, iPad, Apple Watch, Apple TV, and Android. Includes CarPlay and Android Auto, offline downloads, chapter navigation, a sleep timer, and extras support for ePub's & PDF's.',
+  },
+  {
     name: 'plappa',
     platforms: ['iOS', 'iPadOS'],
     href: 'https://plappa.me/',
